cb84bdc72c6cc91ce5367dc4a0f4c2175efe30b1
Yannick Scherer <yannick.scherer@stylefruits.de>
2017-05-09 21:14:20 +0200

    DFS solution.

--- labyrinth/src/labyrinth/solution.clj
+++ labyrinth/src/labyrinth/solution.clj
@@ -47,18 +47,46 @@
           (fn [row]
             (assoc row x 1))))
 
+(defn find-exit
+  [labyrinth]
+  (first
+    (for [x (range 0 (width labyrinth))
+          y (range 0 (height labyrinth))
+          :when (= (tile labyrinth [x y]) :Exit)]
+      [x y])))
+
+(defn distance
+  [[x0 y0] [x1 y1]]
+  (let [delta-x (- x0 x1)
+        delta-y (- y0 y1)]
+    (+ (* delta-x delta-x) (* delta-y delta-y))))
+
+(defn solve-from
+  [labyrinth exit-position position]
+  (let [possible   (->> (possible-moves labyrinth position)
+                        (sort-by #(distance exit-position %)))
+        maybe-exit (first (filter #(= :Exit (tile labyrinth %)) possible))]
+    (cond (empty? possible)
+          nil
+
+          maybe-exit
+          (move-to labyrinth maybe-exit)
+
+          :else
+          (some
+            (fn [selected-move]
+              (solve-from
+                (move-to labyrinth selected-move)
+                exit-position
+                selected-move))
+            possible))))
+
 (defn solve
   [labyrinth]
-  (loop [position  (starting-position labyrinth)
-         labyrinth labyrinth]
-    (let [possible   (possible-moves labyrinth position)
-          maybe-exit (first (filter #(= :Exit (tile labyrinth %)) possible))]
-      (cond (empty? possible)
-            nil
-
-            maybe-exit
-            (move-to labyrinth maybe-exit)
-
-            :else
-            (let [next-position (rand-nth possible)]
-              (recur next-position (move-to labyrinth next-position)))))))
+  (if-let [start (starting-position labyrinth)]
+    (or (solve-from
+          labyrinth
+          (find-exit labyrinth)
+          start)
+        ["unable to exit"])
+    labyrinth))
